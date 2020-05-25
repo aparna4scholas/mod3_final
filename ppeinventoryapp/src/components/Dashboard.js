19d43985280bc296cc992a5bf968468de485d079
aparna4scholas <aparna4scholas@gmail.com>
2020-05-24 21:30:03 -0500

    added data

--- ppeinventoryapp/src/components/Dashboard.js
+++ ppeinventoryapp/src/components/Dashboard.js
@@ -1,13 +1,81 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
- function Dashboard() {
-    
-        return (
-            <div>
-                <h2>list of all details</h2>
-            </div>
-        )
+class Dashboard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {PPECityData: [], isLoading: true};
+    this.remove = this.remove.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({isLoading: true});
+
+    fetch('/ppe_api/v1/ppedata')
+      .then(response => response.json())
+      .then(data => this.setState({PPECityData: data, isLoading: false}));
+  }
+
+  async remove(id) {
+    await fetch(`/ppe_api/v1/ppedata${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      let updatedData = [...this.state.PPECityData].filter(i => i.id !== id);
+      this.setState({PPECityData: updatedData});
+    });
+  }
+
+  render() {
+    const {dashboard, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
     }
 
+    const Citydata = PPECityData.map(dashboard => {
+      return <tr key={dashboard.id}>
+        <td style={{whiteSpace: 'nowrap'}}>{dashboard.firstName}</td>
+        <td>{dashboard.lastName}</td>
+        <td>{dashboard.email}</td>
+        <td>
+          <ButtonGroup>
+            <Button size="sm" color="primary" tag={Link} to={"/employee/" + dashboard.id}>Edit</Button>
+            <Button size="sm" color="danger" onClick={() => this.remove(dashboard.id)}>Delete</Button>
+          </ButtonGroup>
+        </td>
+      </tr>
+    });
+
+    return (
+      <div>
+        <AppNavbar/>
+        <Container fluid>
+          
+          <h3>All cities count</h3>
+          <Table className="mt-4">
+            <thead>
+            <tr>
+              <th width="20%">cityName</th>
+              <th width="20%">MaskCount</th>
+              <th width="20%"> GlovesCount</th>
+              <th width="20%">surgicalGownCount</th>
+              <th width="20%">totalCount</th>
+            </tr>
+            </thead>
+            <tbody>
+            {Dashboard}
+            </tbody>
+          </Table>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
--- ppeinventoryapp/src/components/Navbar.js
+++ ppeinventoryapp/src/components/Navbar.js
@@ -4,22 +4,23 @@ import { Link } from 'react-router-dom';
 
 function Navbar () {
     const navStyle = {
-        color: 'white'
+        color: 'crimson'
     };
 
 return(
 
   
   <nav>
-   <h1>  PPE inventory </h1>
+   <h1> PersonalProtectiveEquipmemt  </h1>
+   
    <ul className="navbar_link">
        <Link style={navStyle} to='/Dashboard'>
-       <li> Dashboard</li>
+       <li> Citydata</li>
        </Link>
        <Link  style={navStyle} to='/Gloves'>
        <li> Gloves</li>
        </Link>
-       <Link style={navStyle} to='Masks'>
+       <Link style={navStyle} to='/Masks'>
        <li> Masks</li>
        </Link >
         </ul>

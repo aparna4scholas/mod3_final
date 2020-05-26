aed917b5bbb1319397950277ade141945b01790c
aparna4scholas <aparna4scholas@gmail.com>
2020-05-26 01:45:18 -0500

    modified

--- ppeinventoryapp/src/components/Dashboard.js
+++ ppeinventoryapp/src/components/Dashboard.js
@@ -4,38 +4,50 @@ import { Link } from 'react-router-dom';
 
 class Dashboard extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {dashboard: {}, isLoading: true};
-  }
-
-  componentDidMount() {
-    this.setState({isLoading: true});
-
-    fetch('/ppe_api/v1/dashboard')
-      .then(response => response.json())
-      .then(data => this.setState({dashboard: data, isLoading: false}));
-  }
-
-  
-
-  render() {
-    const {dashboard, isLoading} = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
+    constructor(props) {
+        super(props);
+        this.state = { dashboard: {}, isLoading: true };
     }
 
-    return (
-      <div>
-       <table><tr> 
-        <td>{dashboard.maskCount}</td>
-        <td>{dashboard.glovesCount}</td>
-        <td>{dashboard.surgicalGownCount}</td>
-        </tr>
-        </table>
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.setState({ isLoading: true });
+
+        fetch('/ppe_api/v1/dashboard')
+            .then(response => response.json())
+            .then(data => this.setState({ dashboard: data, isLoading: false }));
+    }
+
+
+
+    render() {
+        const { dashboard, isLoading } = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div>
+                <div className="card bg-success text-white h-100">
+                    <div className="card-body bg-success">
+                        <h6> mask count</h6>
+                        <h1>{dashboard.maskCount}</h1>
+                    </div>
+                </div>
+                <div className="card bg-success text-white h-100">
+                    <div className="card-body bg-success">
+                        <h6> Gloves count</h6>
+                        <h1>{dashboard.glovesCount}</h1>
+                    </div>
+                </div>
+                <div className="card bg-success text-white h-100">
+                    <div className="card-body bg-success">
+                        <h6> Surgical Gown count</h6>
+                        <h1>{dashboard.surgicalGownCount}</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 export default Dashboard;
